e232ff775675016b48504d6adc0b246e42883470
Meigo <73817505+meigoc@users.noreply.github.com>
2024-07-31 22:21:01 +0300

    New Class: microsoft\windows\api

---
+++ microsoft/windows/WindowsException.php
@@ -0,0 +1,10 @@
+<?php
+namespace microsoft\windows;
+
+/**
+ * Класс исключения, выбрасываемого функциями пакета Windows
+ * Class WindowsException
+ * @package microsoft\windows
+ * @packages framework
+ */
+class WindowsException extends \Exception{}
---
+++ microsoft/windows/result/wshResult.php
@@ -0,0 +1,45 @@
+<?php
+namespace microsoft\windows\result;
+
+use php\lib\str;
+use php\lang\Process;
+
+/**
+ * Class wshResult
+ * @package microsoft\windows
+ * @packages framework
+ */
+class wshResult
+{
+    /**
+     * @var Process
+     */
+    private $process;
+    private $charset = null;
+    
+    public function __construct(Process $process){
+        $this->process = $process;
+    }
+    
+    public function setCharset($charset){
+        $this->charset = $charset;
+        return $this;
+    }
+    
+    public function getOutput(){
+        return $this->decodeStream($this->process->getInput());
+    }    
+
+    public function getError(){
+        return $this->decodeStream($this->process->getError());
+    }
+    
+    private function decodeStream($stream){
+        $data = $stream->readFully();
+        if($this->charset != null){
+            $data = str::decode($data, $this->charset);
+        }
+        
+        return str::trim($data);
+    }
+}
